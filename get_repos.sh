f329412ac269874cf2de8aadcd314c4aad96d5af
Ubuntu <ubuntu@ip-172-31-45-17.ec2.internal>
2018-06-29 19:05:58 +0000

    add script

---
+++ get_repos.sh
@@ -0,0 +1,4 @@
+git clone https://github.com/Sage-Bionetworks/fastq_mixer
+git clone https://github.com/Sage-Bionetworks/kallisto_cwl
+git clone https://github.com/Sage-Bionetworks/synapse_python_client_cwl
+git clone https://github.com/Sage-Bionetworks/misc_cwl
